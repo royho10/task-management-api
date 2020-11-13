65c1d294c0406d6ae59028af2590fcbb5583ad0e
Roy Honigman <royho10@gmail.com>
2020-11-13 11:10:18 +0200

    initial database

---
+++ database.sql
@@ -0,0 +1,22 @@
+/* Run this code in your SQL shell to create the tables */
+
+CREATE TABLE users (
+	user_id BIGSERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	joined TIMESTAMP NOT NULL 
+);
+
+CREATE TABLE login (
+	login_id BIGSERIAL NOT NULL PRIMARY KEY,
+	email TEXT REFERENCES users (email) NOT NULL UNIQUE,
+	hash VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE lists (
+	list_id BIGSERIAL NOT NULL PRIMARY KEY,
+	email TEXT REFERENCES users (email) NOT NULL,
+	list_count BIGSERIAL NOT NULL AUTO_INCREMENT,
+	title VARCHAR(100) NOT NULL
+);
