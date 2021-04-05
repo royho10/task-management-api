ae54404d745278b1d329b75a8fc29d9f03c71ed5
Roy Honigman <royho10@gmail.com>
2021-04-05 23:33:09 +0300

    fix knex connection

--- server.js
+++ server.js
@@ -19,19 +19,19 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 // connecting the database to the server
 const db = knex({
-	client: 'pg',
+	/*client: 'pg',
 	connection: {
 		host: '127.0.0.1',
 		user: 'postgres',
 		password: '',
 		database: 'taskmanagement'
 	}
-	/*
+	*/
 	connection: {
 		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
-	*/
+	
 });
 
 const app = express();
